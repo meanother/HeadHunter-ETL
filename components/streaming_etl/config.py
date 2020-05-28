0df7e86182ad8c9cdc9c1b7bbce65f5dbccf2c7f
arty <juicehq@yandex.ru>
2020-05-28 22:01:20 +0300

    delete config file

--- components/streaming_etl/config.py
+++
@@ -1,9 +0,0 @@
-
-log_file = '/home/arty/python/hh-database-etl/etl_logfile.log'
-
-class Config:
-    database_url = '77.244.65.15:58642'
-    database_login = 'semenov'
-    database_password = '12345'
-    database_name = 'parsing_db'
-    database_type = 'postgresql'
